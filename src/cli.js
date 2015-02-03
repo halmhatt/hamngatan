dd1115dbfe93abee6dc8d8707e55c44f04ca584f
Jacob Carlsson <jacob.carlsson@gmail.com>
2015-02-03 09:34:44 +0100

    Update CLI with es6 syntax

--- src/cli.js
+++ src/cli.js
@@ -2,8 +2,8 @@
 
 import 'core-js/shim';
 
-var program = require('commander');
-var Hamngatan = require('./hamngatan.js');
+import program from'commander';
+import Hamngatan from './hamngatan.js';
 
 program
 	.version('1.0.0')
@@ -12,12 +12,19 @@ program
 	.option('--to [date]', 'To date')
 	.parse(process.argv);
 
+// If no argument is given, output help
+if(process.argv.length === 2) {
+	program.help();
+}
+
+// API key needs to be present
 if(!program.apikey) {
 	console.error('You need to specify an apikey');
+	console.error('Obtain one at: http://www.linkoping.se/open/Oppna-data1/Luftkvalitet/');
 	return;
 }
 
-var options = {};
+let options = {};
 if(program.from) {
 	options.from = program.from;
 } else {
@@ -29,7 +36,7 @@ if(program.to) {
 	options.to = program.to;
 }
 
-var hamngatan = new Hamngatan(program.apikey);
+let hamngatan = new Hamngatan(program.apikey);
 
 // Output everything as JSON
 hamngatan.get(options)
